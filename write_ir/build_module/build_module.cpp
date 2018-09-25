80de7b4c6a6764fb9b8b31d0da627dc9bd4c992f
zhangshuai <zhangshuaiisme@gmail.com>
2018-09-25 16:05:56 +0800

    IR 1: The ir of build_module can print hello world

--- write_ir/build_module/build_module.cpp
+++ write_ir/build_module/build_module.cpp
@@ -9,6 +9,9 @@
 #include "llvm/IR/Type.h"           //标准类型，int8/16.. pointer
 #include "llvm/IR/Verifier.h"
 
+//字节码
+#include "llvm/Bitcode/BitcodeWriter.h"
+
 //c++标准库
 #include <iostream>
 #include <algorithm>
@@ -34,22 +37,19 @@ int main(){
 //构建Module
     llvm::Module my_module("My_Module",TheContext);
 
-//全局变量 char str[]="hello world!\n"
-    //类型问题：string = int8 指针
+//全局变量
     PointerType * str_type = Type::getInt8PtrTy(TheContext);
-    //创建全局字符串：
     Constant *init_str = llvm::ConstantDataArray::getString(TheContext,"Hello World!\n");
-    GlobalVariable  *global_str = new llvm::GlobalVariable(my_module,str_type, true, GlobalValue::PrivateLinkage,init_str,"str");
-    /*
-     * 对于llvm::GlobalVariable函数说明：
-     * Module引用
-     * 变量类型
-     * 是否为常量
-     * 所有权：私有，共享
-     * 初始化的值
-     * 变量名
-     * */
-     
+    GlobalVariable  *global_str = new llvm::GlobalVariable(my_module,str_type, true, GlobalValue::PrivateLinkage,init_str,"global_str");
+    
+//添加 printf 函数声明
+    //返回值类型 printf 返回int32类型
+    vector<Type *> func_argu_type;
+    func_argu_type.push_back(Type::getInt8PtrTy(TheContext));
+    
+    FunctionType * printf_func_type = FunctionType::get(Type::getInt32Ty(TheContext), func_argu_type, true);
+    Function * printf_func = Function::Create(printf_func_type, GlobalValue::ExternalLinkage, "printf", &my_module);
+
 //添加 main Function
     //返回值类型
     IntegerType * result_type = Type::getInt32Ty(TheContext);
@@ -62,46 +62,34 @@ int main(){
     //创建function,并插入Module
     Function * main_func = Function::Create(main_func_type, GlobalValue::ExternalLinkage, "main", &my_module);
 
-//添加 printf 函数声明
-    //返回值类型 printf 返回int32类型
-    FunctionType * printf_func_type = FunctionType::get(result_type,  ,true);
-    Function * main_func = Function::Create(main_func_type, GlobalValue::ExternalLinkage, "main", &my_module);
-
 //添加basicblock
     //创建BasicBlock，并插入函数
     BasicBlock * main_bb = BasicBlock::Create(TheContext, "main_bb", main_func, nullptr);
-    //
     
-
-//添加指令
-    //设置插入点 为main_bb
+    //设置插入点 main_bb末尾
     Builder.SetInsertPoint(main_bb);
+
+//局部变量 char str[]="hello world!\n"
+    Value * local_str = Builder.CreateGlobalStringPtr("hello world!\n","str");
+
+//添加 call printf 指令
+    //参数
+    vector<Value *> func_argu;
+    func_argu.push_back(local_str);
+    //func_argu.push_back(global_str);
+    //插入 call printf 指令
+    Builder.CreateCall(printf_func, func_argu);
+
+
+//添加 ret int32 指令
     //插入 return int32 指令
     Builder.CreateRet(Builder.getInt32(0));
+
+//确认
+    verifyModule(my_module);
     
-/*
-//添加函数调用指令
-    //查找函数是否在Module中,不在就报错
-    Function *Call_printf = TheModule->getFunction("printf");
-    if (!Call_printf)
-        cout<<"Unknown function referenced"<<endl;;
-
-    //参数不匹配错误
-    if (Call_printf->arg_size() != Args.size())
-        return LogErrorV("Incorrect # arguments passed");
-
-    vector<Value *> ArgsV;
-    for (unsigned i = 0, e = Args.size(); i != e; ++i) {
-        ArgsV.push_back(Args[i]->codegen());
-        if (!ArgsV.back())
-            return nullptr;
-    }
-    //创建调用 call 指令
-    Builder.CreateCall(Call_printf, ArgsV, "call_printf");
-
-//添加ret指令
-*/
 
 //打印IR，用来调试
     my_module.dump();
+    //WriteBitcodeToFile(&my_module, outs());
 }
